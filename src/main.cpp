a7315ecf24b06aef79394b50191ff031ee5d201e
Ihor Metelytsia <ihor.metelytsia@gmail.com>
2026-04-24 23:33:35 +0300

    Update main elements

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,22 @@
-#include <QCoreApplication>
-#include <QThread>
-#include <QTimer>
-#include <QDebug>
-#include "task_queue.h"
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "threadcontroller.h"
 
 int main(int argc, char* argv[])
 {
-    QCoreApplication app(argc, argv);
+    QGuiApplication app(argc, argv);
 
-    // Створюємо Worker і Thread окремо
-    auto* queue  = new TaskQueue();
-    auto* thread = new QThread();
+    ThreadController controller;
 
-    // Переміщуємо Worker в новий потік.
-    // Після цього всі слоти queue виконуватимуться в thread.
-    queue->moveToThread(thread);
+    QQmlApplicationEngine engine;
 
-    QObject::connect(thread, &QThread::started, queue, &TaskQueue::process);
-    QObject::connect(queue, &TaskQueue::allDone, thread, &QThread::quit);
-    QObject::connect(thread, &QThread::finished, queue, &QObject::deleteLater);
-    QObject::connect(thread, &QThread::finished, thread, &QObject::deleteLater);
-    QObject::connect(queue, &TaskQueue::taskCompleted,
-                     [](int id, const QString& r) {
-                         qDebug() << "Task" << id << ":" << r;
-                     });
+    qmlRegisterType<ThreadController>("ThreadingDemo", 1, 0, "ThreadController");
 
-    // Стартуємо потік
-    thread->start();
+    engine.loadFromModule("ThreadingDemo.UI", "Main");
 
-    // Додаємо задачі в чергу
-    queue->enqueue({"test.txt", 1});
-    queue->enqueue({"test2.txt", 2});
-    queue->enqueue({"test3.txt", 3});
-
-    QTimer::singleShot(800, [queue]() { // симуляція зупинки
-        qDebug() << "[main timer single shot]" << QThread::currentThread() << "Stop queue!";
-        queue->stop();
-    });
-
-    QObject::connect(thread, &QThread::finished, &app, &QCoreApplication::quit);
+    if (engine.rootObjects().isEmpty())
+        return -1;
 
     return app.exec();
 }
